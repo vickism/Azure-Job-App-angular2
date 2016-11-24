dd405fd81693d4930760577ffdc512156ea9a0a4
vick tahilramani <vick.tahilramani@readify.net>
2016-11-24 12:07:19 +1100

    Added typings

---
+++ typings.d.ts
@@ -0,0 +1,10 @@
+// Typings reference file, you can add your own global typings here
+// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+
+{
+  "globalDependencies": {
+    "core-js": "registry:dt/core-js#0.0.0+20160914114559",
+    "jasmine": "registry:dt/jasmine#2.5.0+20161025102649",
+    "node": "registry:dt/node#6.0.0+20161110151007"
+  }
+}
